0309ff27136675bca256d555fe6ef11099051d7c
yamidevs <yamisaad@live.fr>
2014-06-23 20:44:09 +0100

    realm términer

---
+++ yEmu/Realm/Databases/Requetes/GameServers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yEmu.Realm.Classes;
+using yEmu.Util;
+using Dapper;
+
+
+namespace yEmu.Realm.Databases.Requetes
+{
+    class GameServers
+    {
+        public static List<GameServer> servers = new List<GameServer>();
+
+        public static void LoadServers()
+        {
+            using (IDbConnection connection = Databases.GetConnection())
+            {
+
+                var server = connection.Query<GameServer>("SELECT * FROM gameservers");
+                servers.AddRange(server);
+
+            }
+            Info.Write("", string.Format("{0} Servers chargés", servers.Count()), ConsoleColor.Green);
+        }
+    }
+}
---
+++ yEmu/Realm/Packets/Auth/ErrorAuth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yEmu.Realm.Packets.Auth
+{
+    sealed class ErrorAuth : IPacket
+    {
+        public override string ToString()
+        {
+            return "AlEw";
+        }
+    }
+}
